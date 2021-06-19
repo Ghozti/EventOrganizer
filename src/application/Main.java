54979738e66214d801bd142c037724d6c13841bb
Ghozti <ybalderas1005@gmail.com>
2021-06-18 21:00:47 -0400

    - worked on RegisterController.java

--- src/application/Main.java
+++ src/application/Main.java
@@ -11,7 +11,7 @@ public class Main extends Application {
     @Override
     public void start(Stage primaryStage) throws Exception{
         Parent root = FXMLLoader.load(getClass().getResource("fxlm/login.fxml"));
-        primaryStage.setTitle("Hello World");
+        primaryStage.setTitle("Event Manager");
         primaryStage.setScene(new Scene(root, 600, 400));
         primaryStage.setResizable(false);
         primaryStage.show();
--- src/application/controllers/RegisterController.java
+++ src/application/controllers/RegisterController.java
@@ -1,12 +1,53 @@
 package application.controllers;
 
 import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.PasswordField;
+
+import java.awt.*;
 
 public class RegisterController {
 
     private final String passErrLabel = "passwords did not match", successLabel = "Success! you may now leave this page";
 
+    @FXML
+    javafx.scene.control.TextField name;
+
+    @FXML
+    PasswordField pass;
+
+    @FXML
+    PasswordField cPass;
+
+    @FXML
+    Label passErrLbl, continueLbl;
+
     public void continueRegistration(ActionEvent event){
 
+        if(!getPass().equals(getCPass())){
+            setPassErrLbl();
+        }else {
+            setContinueLbl();
+        }
+    }
+
+    String getName(){
+        return name.getText();
+    }
+
+    String getPass(){
+        return pass.getText();
+    }
+
+    String getCPass(){
+        return cPass.getText();
+    }
+
+    void setPassErrLbl(){
+        passErrLbl.setText(passErrLabel);
+    }
+
+    void setContinueLbl(){
+        continueLbl.setText(successLabel);
     }
 }
