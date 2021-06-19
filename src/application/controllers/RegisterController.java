33538300a1b9c28e8a5a9917517c5ae96622d1dc
Ghozti <ybalderas1005@gmail.com>
2021-06-18 20:54:06 -0400

    - added the controller to register.fxml - filled in RegisterController.java

--- src/application/controllers/RegisterController.java
+++ src/application/controllers/RegisterController.java
@@ -1,8 +1,12 @@
 package application.controllers;
 
+import javafx.event.ActionEvent;
+
 public class RegisterController {
 
     private final String passErrLabel = "passwords did not match", successLabel = "Success! you may now leave this page";
 
+    public void continueRegistration(ActionEvent event){
 
+    }
 }
