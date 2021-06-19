3e6f62caf48d43f8a72bba472e7b635646b2f1ad
Ghozti <ybalderas1005@gmail.com>
2021-06-18 20:49:18 -0400

    - worked on register.fxml

--- src/application/controllers/RegisterController.java
+++ src/application/controllers/RegisterController.java
@@ -1,4 +1,8 @@
 package application.controllers;
 
 public class RegisterController {
+
+    private final String passErrLabel = "passwords did not match", successLabel = "Success! you may now leave this page";
+
+
 }
